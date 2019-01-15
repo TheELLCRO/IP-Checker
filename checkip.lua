299e711e089b4b3752ff39fc5d2d4f7a8be99885
pavlik220205 <44651742+pavlik220205@users.noreply.github.com>
2019-01-16 00:50:55 +0200

    init

---
+++ checkip.lua
@@ -0,0 +1,133 @@
+script_name("IP Checker")
+script_author("drags (blast.hk)")
+script_version("Poxuy na Versiyu")
+local cjson = require"cjson"
+local encoding = require"encoding"
+local effil = require"effil"
+encoding.default = "cp1251"
+u8 = encoding.UTF8
+
+function asyncHttpRequest(method, url, args, resolve, reject)
+	local request_thread = effil.thread(function(method, url, args)
+		local requests = require"requests"
+		local result, response = pcall(requests.request, method, url, args)
+		if result then
+			response.json, response.xml = nil, nil
+			return true, response
+		else
+			return false, response
+		end
+	end)(method, url, args)
+
+	if not resolve then
+		resolve = function() end
+	end
+	if not reject then
+		reject = function() end
+	end
+	lua_thread.create(function()
+		local runner = request_thread
+		while true do
+			local status, err = runner:status()
+			if not err then
+				if status == "completed" then
+					local result, response = runner:get()
+					if result then
+						resolve(response)
+					else
+						reject(response)
+					end
+					return
+				elseif status == "canceled" then
+					return reject(status)
+				end
+			else
+				return reject(err)
+			end
+			wait(0)
+		end
+	end)
+end
+
+function distance_cord(lat1, lon1, lat2, lon2)
+	if lat1 == nil or lon1 == nil or lat2 == nil or lon2 == nil or lat1 == "" or lon1 == "" or lat2 == "" or lon2 == "" then
+		return 0
+	end
+	local dlat = math.rad(lat2 - lat1)
+	local dlon = math.rad(lon2 - lon1)
+	local sin_dlat = math.sin(dlat / 2)
+	local sin_dlon = math.sin(dlon / 2)
+	local a =
+		sin_dlat * sin_dlat + math.cos(math.rad(lat1)) * math.cos(
+			math.rad(lat2)
+		) * sin_dlon * sin_dlon
+	local c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+	local d = 6378 * c
+	return d
+end
+
+function main()
+	if not isSampLoaded() or not isSampfuncsLoaded() then return end
+	while not isSampAvailable() do
+		wait(100)
+	end
+	sampRegisterChatCommand("chip", chip)
+	wait(-1)
+end
+
+function chip(cl)
+	ips = {}
+	for word in string.gmatch(cl, "(%d+%p%d+%p%d+%p%d+)") do
+		table.insert(ips, { query = word })
+	end
+	if #ips > 0 then
+		data_json = cjson.encode(ips)
+		asyncHttpRequest(
+			"POST",
+			"http://ip-api.com/batch?fields=25305&lang=ru",
+			{ data = data_json },
+			function(response)
+				local rdata = cjson.decode(u8:decode(response.text))
+				local text = ""
+				for i = 1, #rdata do
+					if rdata[i]["status"] == "success" then
+						local distances =
+							distance_cord(
+								rdata[1]["lat"],
+								rdata[1]["lon"],
+								rdata[i]["lat"],
+								rdata[i]["lon"]
+							)
+						text =
+							text .. string.format(
+								"\n{FFF500}IP - {FF0400}%s\n{FFF500}Страна -{FF0400} %s\n{FFF500}Город -{FF0400} %s\n{FFF500}Провайдер -{FF0400} %s\n{FFF500}Растояние -{FF0400} %d  \n\n",
+								rdata[i]["query"],
+								rdata[i]["country"],
+								rdata[i]["city"],
+								rdata[i]["isp"],
+								distances
+							)
+               end
+				end
+				if text == "" then
+					text = " \n\t{FFF500}Ничего не найдено"
+				end
+				showdialog("Информация о IP", text)
+			end,
+			function(err)
+				showdialog("Информация о IP", "Произошла ошибка \n" .. err)
+			end
+		)
+	end
+end
+
+function showdialog(name, rdata)
+	sampShowDialog(
+		math.random(1000),
+		"{FF4444}" .. name,
+		rdata,
+		"Закрыть",
+		false,
+		0
+	)
+end
